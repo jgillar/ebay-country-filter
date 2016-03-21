fd3ebd9e443d6a6cf11156766969e447f3605211
jgillar <jamie.gillar@gmail.com>
2016-03-21 18:44:41 -0400

    got rid of metadata file because I decided to put this on greasyfork and give updates through that

--- ecf.user.js
+++ ecf.user.js
@@ -1,12 +1,10 @@
 // ==UserScript==
 // @name         ebay Country Filter
 // @namespace    https://greasyfork.org/
-// @version      0.2.3
+// @version      0.2.4
 // @description  Attempts to clear up unwanted items in your ebay search results
 // @author       Schabernack
 // @match        http://www.ebay.com/sch/i.html*
-// @updateURL	 https://raw.githubusercontent.com/jgillar/ebay-country-filter/master/ecf.meta.js	
-// @downloadURL  https://raw.githubusercontent.com/jgillar/ebay-country-filter/master/ecf.user.js
 // @grant        none
 // ==/UserScript==
 

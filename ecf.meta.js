c7b9e3ef7f0ea3ac9b54f80814f298ad7c5f4fc1
jgillar <jamie.gillar@gmail.com>
2016-03-21 04:34:07 -0400

    added metadata file

---
+++ ecf.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         ebay Country Filter
+// @namespace    https://greasyfork.org/
+// @version      0.2.2
+// @description  Attempts to clear up unwanted items in your ebay search results
+// @author       Schabernack
+// @match        http://www.ebay.com/sch/i.html*
+// @updateURL	 https://raw.githubusercontent.com/jgillar/ebay-country-filter/master/ecf.meta.js	
+// @downloadURL  https://raw.githubusercontent.com/jgillar/ebay-country-filter/master/ecf.user.js
+// @grant        none
+// ==/UserScript==
